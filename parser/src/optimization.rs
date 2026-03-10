0f2ccaea02239e3d3a4dabc0f0aee9df1901a308
FreePlacki <contact@pbielecki.com>
2026-03-10 13:42:44 +0100

    Add some doc comments

--- parser/src/optimization.rs
+++ parser/src/optimization.rs
@@ -3,10 +3,15 @@ use quick_xml::events::BytesStart;
 use crate::{error::ParseError, utils::parse_value};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
+/// optimization weights: weights on the total penalty of the solution
 pub struct Optimization {
+    /// penalty for assigned times
     pub time: i32,
+    /// penalty for assigned rooms
     pub room: i32,
+    /// penalty for violated soft distribution constraints
     pub distribution: i32,
+    /// penalty for student conflicts
     pub student: i32,
 }
 
