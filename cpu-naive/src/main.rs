0b7b6815268a4aa44985393fc2bca8ed4dd27e0e
Kamil Błażejczyk <01189072@pw.edu.pl>
2026-03-20 18:14:19 +0100

    Add Solver skeleton

--- cpu-naive/src/main.rs
+++ cpu-naive/src/main.rs
@@ -1,9 +1,11 @@
 use anyhow::{Result, bail};
 use std::{env, fs};
-
+use rand::rng;
 use parser::problem::Problem;
+use crate::solver::{NaiveSolver, Solver};
 
 mod model;
+mod solver;
 
 fn main() -> Result<()> {
     let args: Vec<_> = env::args().collect();
@@ -13,7 +15,11 @@ fn main() -> Result<()> {
 
     let input = fs::read_to_string(&args[1])?;
     let problem = Problem::parse(input)?;
-    println!("{:#?}", problem);
+    // println!("{:#?}", problem);
+    
+    let mut solver = NaiveSolver::new(Box::new(rng()), 1, 1, problem);
+    let solution = solver.solve();
+    dbg!(solution);
 
     Ok(())
 }
