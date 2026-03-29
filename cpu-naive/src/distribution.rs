8ec1ca0933238dd7415c853279d706dbc171454e
Kamil Blazejczyk <kamileczus@gmail.com>
2026-03-29 15:08:25 +0200

    Refactor fitness summing

--- cpu-naive/src/distribution.rs
+++ cpu-naive/src/distribution.rs
@@ -1,6 +1,6 @@
-﻿use std::ops::AddAssign;
+﻿use parser::distributions::DistributionKind;
 use crate::fitness::Fitness;
-use crate::model::TimetableData;
+use crate::model::{DistributionData, TimetableData};
 use crate::solution::Solution;
 
 pub(crate) struct Distribution<'a> {
@@ -17,102 +17,106 @@ impl<'a> Distribution<'a> {
     pub fn calculate_penalty(self) -> Fitness {
         let mut fitness = Fitness::new();
 
-        fitness += self.same_start();
-        fitness += self.same_time();
-        fitness += self.different_time();
-        fitness += self.same_days();
-        fitness += self.different_days();
-        fitness += self.same_weeks();
-        fitness += self.different_weeks();
-        fitness += self.overlap();
-        fitness += self.not_overlap();
-        fitness += self.same_room();
-        fitness += self.different_room();
-        fitness += self.same_attendees();
-        fitness += self.precedence();
-        fitness += self.work_day();
-        fitness += self.min_gap();
-        fitness += self.max_days();
-        fitness += self.max_day_load();
-        fitness += self.max_breaks();
-        fitness += self.max_block();
+        self.data.distributions.iter().for_each(|d| {
+            fitness += match d.kind {
+                DistributionKind::SameStart => self.same_start(d),
+                DistributionKind::SameTime => self.same_time(d),
+                DistributionKind::DifferentTime => self.different_time(d),
+                DistributionKind::SameDays => self.same_days(d),
+                DistributionKind::DifferentDays => self.different_days(d),
+                DistributionKind::SameWeeks => self.same_weeks(d),
+                DistributionKind::DifferentWeeks => self.different_weeks(d),
+                DistributionKind::Overlap => self.overlap(d),
+                DistributionKind::NotOverlap => self.not_overlap(d),
+                DistributionKind::SameRoom => self.same_room(d),
+                DistributionKind::DifferentRoom => self.different_room(d),
+                DistributionKind::SameAttendees => self.same_attendees(d),
+                DistributionKind::Precedence => self.precedence(d),
+                DistributionKind::WorkDay(_) => self.work_day(d),
+                DistributionKind::MinGap(_) => self.min_gap(d),
+                DistributionKind::MaxDays(_) => self.max_days(d),
+                DistributionKind::MaxDayLoad(_) => self.max_day_load(d),
+                DistributionKind::MaxBreaks(_, _) => self.max_breaks(d),
+                DistributionKind::MaxBlock(_, _) => self.max_block(d),
+            }
+        });
 
         fitness
     }
 
-    fn same_start(&self) -> Fitness {
+    fn same_start(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn same_time(&self) -> Fitness {
+    fn same_time(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn different_time(&self) -> Fitness {
+    fn different_time(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn same_days(&self) -> Fitness {
+    fn same_days(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn different_days(&self) -> Fitness {
+    fn different_days(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn same_weeks(&self) -> Fitness {
+    fn same_weeks(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn different_weeks(&self) -> Fitness {
+    fn different_weeks(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn overlap(&self) -> Fitness {
+    fn overlap(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn not_overlap(&self) -> Fitness {
+    fn not_overlap(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn same_room(&self) -> Fitness {
+    fn same_room(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn different_room(&self) -> Fitness {
+    fn different_room(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn same_attendees(&self) -> Fitness {
+    fn same_attendees(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn precedence(&self) -> Fitness {
+    fn precedence(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn work_day(&self) -> Fitness {
+    fn work_day(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn min_gap(&self) -> Fitness {
+    fn min_gap(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn max_days(&self) -> Fitness {
+    fn max_days(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn max_day_load(&self) -> Fitness {
+    fn max_day_load(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn max_breaks(&self) -> Fitness {
+    fn max_breaks(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 
-    fn max_block(&self) -> Fitness {
+    fn max_block(&self, dist: &DistributionData) -> Fitness {
         todo!()
     }
 }
